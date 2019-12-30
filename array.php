b77d2cce8a4fd8d41e4c3974e35c15d53bedd720
Song ha <soeng.cotafer12@gmail.com>
2019-12-30 10:55:07 +0700

    add

---
+++ array.php
@@ -0,0 +1,11 @@
+<?php
+    $color = array ("Red","Green","Blue");
+    var_dump($color);
+    echo "<br>";
+
+    $color_codes = array(
+        "Red"   => "#ff0000",
+        "Green" =>  "#00ff00",
+        "Blue"  =>  "#000ff"
+    );
+    var_dump($color_codes);
